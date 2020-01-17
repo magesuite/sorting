bf62c177f684f1c0510073d5d9f24ee777be9ba8
Błażej Dołęska <blazej.doleska@creativestyle.pl>
2020-01-17 10:05:40 +0000

    added logic to retrieve only sorting options selected for specific category

--- Plugin/Catalog/Block/Product/ProductList/Toolbar/ReplaceAvailableOrders.php
+++ Plugin/Catalog/Block/Product/ProductList/Toolbar/ReplaceAvailableOrders.php
@@ -9,18 +9,55 @@ class ReplaceAvailableOrders
      * @var \MageSuite\Sorting\Model\Sorting\Options
      */
     protected $options;
+    /**
+     * @var \Magento\Framework\Registry
+     */
+    protected $registry;
 
-    public function __construct(\MageSuite\Sorting\Model\Sorting\Options $options)
+    public function __construct(
+        \MageSuite\Sorting\Model\Sorting\Options $options,
+        \Magento\Framework\Registry $registry
+    )
     {
         $this->options = $options;
+        $this->registry = $registry;
     }
 
     public function aroundGetAvailableOrders(\Magento\Catalog\Block\Product\ProductList\Toolbar $subject, callable $proceed)
     {
+        $availableSortBy = [];
+
+        $allSortableAttributes = $this->getAllSortableAttributes();
+
+        $currentCategory = $this->registry->registry('current_category');
+        if (!$currentCategory->getAvailableSortBy()) {
+            return $allSortableAttributes;
+        }
+
+        foreach ($currentCategory->getAvailableSortBy() as $sortBy) {
+            if (!isset($allSortableAttributes[$sortBy])) {
+                continue;
+            }
+
+            $availableSortBy[$sortBy] = $allSortableAttributes[$sortBy];
+        }
+
+        if (empty($availableSortBy)) {
+            return $allSortableAttributes;
+        }
+
+        return $availableSortBy;
+    }
+
+    protected function getAllSortableAttributes()
+    {
         $options = [];
+
+        /* @var $attribute \Magento\Eav\Model\Entity\Attribute\AbstractAttribute */
         foreach ($this->options->getOptions() as $option) {
             $options[$option['value']] = $option['label'];
         }
+
         return $options;
     }
 }
