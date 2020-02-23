f9d2dc4cc0ec4d4e08744261d635409cb3e0c0c5
Mateusz Krzeszowiak <mateusz.krzeszowiak@creativestyle.pl>
2020-02-23 16:32:11 +0100

    Remove jQuery UI from direct dependencies

--- view/frontend/requirejs-config.js
+++ view/frontend/requirejs-config.js
@@ -1,7 +1,14 @@
 var config = {
-    map: {
-        '*': {
-            productListToolbarForm: 'MageSuite_Sorting/js/product/list/toolbar'
-        }
-    }
+    // map: {
+    //     '*': {
+    //         productListToolbarForm: 'MageSuite_Sorting/js/product/list/toolbar'
+    //     }
+    // }
+    config: {
+        mixins: {
+            'Magento_Catalog/js/product/list/toolbar': {
+                'MageSuite_Sorting/js/product/list/toolbar': true,
+            },
+        },
+    },
 };
--- view/frontend/web/js/product/list/toolbar.js
+++ view/frontend/web/js/product/list/toolbar.js
@@ -1,142 +1,140 @@
-define([
-    'jquery',
-    'jquery/ui'
-], function ($) {
+define(['jquery'], function($) {
     'use strict';
-    $.widget('mage.productListToolbarForm', {
 
-        options: {
-            modeControl: '[data-role="mode-switcher"]',
-            directionControl: '[data-role="direction-switcher"]',
-            orderControl: '[data-role="sorter"]',
-            limitControl: '[data-role="limiter"]',
-            mode: 'product_list_mode',
-            direction: 'product_list_dir',
-            order: 'product_list_order',
-            limit: 'product_list_limit',
-            modeDefault: 'grid',
-            directionDefault: 'asc',
-            orderDefault: 'position',
-            limitDefault: '9',
-            url: ''
-        },
+    return function(productListToolbarForm) {
+        $.widget('mage.productListToolbarForm', productListToolbarForm, {
+            /** @inheritdoc */
+            _create: function() {
+                this._bind(
+                    $(this.options.modeControl),
+                    this.options.mode,
+                    this.options.modeDefault
+                );
+                this._bind(
+                    $(this.options.limitControl),
+                    this.options.limit,
+                    this.options.limitDefault
+                );
+                this._bindOrder($(this.options.orderControl));
+            },
 
-        /** @inheritdoc */
-        _create: function () {
-            this._bind($(this.options.modeControl), this.options.mode, this.options.modeDefault);
-            this._bind($(this.options.limitControl), this.options.limit, this.options.limitDefault);
-            this._bindOrder($(this.options.orderControl));
-        },
+            /** @inheritdoc */
+            _bindOrder: function(element) {
+                var eventType = 'change';
 
-        /** @inheritdoc */
-        _bind: function (element, paramName, defaultValue) {
-            if (element.is('select')) {
-                element.on('change', {
-                    paramName: paramName,
-                    'default': defaultValue
-                }, $.proxy(this._processSelect, this));
-            } else {
-                element.on('click', {
-                    paramName: paramName,
-                    'default': defaultValue
-                }, $.proxy(this._processLink, this));
-            }
-        },
-
-        /** @inheritdoc */
-        _bindOrder: function (element) {
-            var eventType = 'change';
-            
-            if (!element.is('select')) {
-                eventType = 'click';
-            }
-            element.on(eventType, {}, $.proxy(this._processSorting, this));
-        },
-
-        /**
-         * @param {jQuery.Event} event
-         * @private
-         */
-        _processLink: function (event) {
-            event.preventDefault();
-
-            this.changeUrl([{
-                "name": event.data.paramName,
-                "default": event.data.default,
-                "value": $(event.currentTarget).data('value')
-            }]);
-        },
-
-        /**
-         * @param {jQuery.Event} event
-         * @private
-         */
-        _processSelect: function (event) {
-            this.changeUrl([{
-                "name": event.data.paramName,
-                "default": event.data.default,
-                "value": event.currentTarget.options[event.currentTarget.selectedIndex].value
-            }]);
-        },
-
-        /**
-         * @param {jQuery.Event} event
-         * @private
-         */
-        _processSorting: function (event) {
-            event.preventDefault();
-            var eventType = event.type,
-                eventTarget = event.currentTarget,
-                sortingDirection,
-                sortingField;
-
-            if (eventType === 'change') {
-                sortingDirection = $(eventTarget.options[eventTarget.selectedIndex]).data('direction');
-                sortingField = eventTarget.options[eventTarget.selectedIndex].value;
-            } else {
-                sortingDirection = $(eventTarget).data('direction');
-                sortingField = $(eventTarget).data('value');
-            }
-            
-            this.changeUrl([
-                {"name": "product_list_order", "default": "", "value": sortingField},
-                {"name": "product_list_dir", "default": "", "value": sortingDirection},
-            ]);
-        },
-
-        /**
-         * Method was modified to allow multiple parameters at once. It is needed to change sorting field and direction
-         * at the same time.
-         * @param {*} params
-         */
-        changeUrl: function (params) {
-            var decode = window.decodeURIComponent,
-                urlPaths = this.options.url.split('?'),
-                baseUrl = urlPaths[0],
-                urlParams = urlPaths[1] ? urlPaths[1].split('&') : [],
-                paramData = {},
-                parameters, i;
-
-            for (i = 0; i < urlParams.length; i++) {
-                parameters = urlParams[i].split('=');
-                paramData[decode(parameters[0])] = parameters[1] !== undefined ?
-                    decode(parameters[1].replace(/\+/g, '%20')) :
-                    '';
-            }
-
-            params.forEach(function (item) {
-                paramData[item.name] = item.value;
-
-                if (item.value == item.default) { //eslint-disable-line eqeqeq
-                    delete paramData[item.name];
+                if (!element.is('select')) {
+                    eventType = 'click';
                 }
-            });
+                element.on(eventType, {}, $.proxy(this._processSorting, this));
+            },
 
-            paramData = $.param(paramData);
+            /**
+             * @param {jQuery.Event} event
+             * @private
+             */
+            _processLink: function(event) {
+                event.preventDefault();
 
-            location.href = baseUrl + (paramData.length ? '?' + paramData : '');
-        }
-    });
+                this.changeUrl([
+                    {
+                        name: event.data.paramName,
+                        default: event.data.default,
+                        value: $(event.currentTarget).data('value'),
+                    },
+                ]);
+            },
 
-    return $.mage.productListToolbarForm;
+            /**
+             * @param {jQuery.Event} event
+             * @private
+             */
+            _processSelect: function(event) {
+                this.changeUrl([
+                    {
+                        name: event.data.paramName,
+                        default: event.data.default,
+                        value:
+                            event.currentTarget.options[
+                                event.currentTarget.selectedIndex
+                            ].value,
+                    },
+                ]);
+            },
+
+            /**
+             * @param {jQuery.Event} event
+             * @private
+             */
+            _processSorting: function(event) {
+                event.preventDefault();
+                var eventType = event.type,
+                    eventTarget = event.currentTarget,
+                    sortingDirection,
+                    sortingField;
+
+                if (eventType === 'change') {
+                    sortingDirection = $(
+                        eventTarget.options[eventTarget.selectedIndex]
+                    ).data('direction');
+                    sortingField =
+                        eventTarget.options[eventTarget.selectedIndex].value;
+                } else {
+                    sortingDirection = $(eventTarget).data('direction');
+                    sortingField = $(eventTarget).data('value');
+                }
+
+                this.changeUrl([
+                    {
+                        name: 'product_list_order',
+                        default: '',
+                        value: sortingField,
+                    },
+                    {
+                        name: 'product_list_dir',
+                        default: '',
+                        value: sortingDirection,
+                    },
+                ]);
+            },
+
+            /**
+             * Method was modified to allow multiple parameters at once. It is needed to change sorting field and direction
+             * at the same time.
+             * @param {*} params
+             */
+            changeUrl: function(params) {
+                var decode = window.decodeURIComponent,
+                    urlPaths = this.options.url.split('?'),
+                    baseUrl = urlPaths[0],
+                    urlParams = urlPaths[1] ? urlPaths[1].split('&') : [],
+                    paramData = {},
+                    parameters,
+                    i;
+
+                for (i = 0; i < urlParams.length; i++) {
+                    parameters = urlParams[i].split('=');
+                    paramData[decode(parameters[0])] =
+                        parameters[1] !== undefined
+                            ? decode(parameters[1].replace(/\+/g, '%20'))
+                            : '';
+                }
+
+                params.forEach(function(item) {
+                    paramData[item.name] = item.value;
+
+                    if (item.value == item.default) {
+                        //eslint-disable-line eqeqeq
+                        delete paramData[item.name];
+                    }
+                });
+
+                paramData = $.param(paramData);
+
+                location.href =
+                    baseUrl + (paramData.length ? '?' + paramData : '');
+            },
+        });
+
+        return $.mage.productListToolbarForm;
+    };
 });
